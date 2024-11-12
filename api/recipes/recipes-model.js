27c927eab5777e9bb78d2bb9d6eddafa1f26488a
Robert Jank <robert.jank@yahoo.com>
2024-11-12 15:08:23 -0600

    wip

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,7 +1,21 @@
 const db = require("../../data/db-config");
 
 async function getRecipeById(recipe_id) {
-  const recipeRows = await db("recipes as r").where("recipe_id", recipe_id);
+  const recipeRows = await db("recipes as r")
+    .leftJoin("steps as s", "r.recipe_id", "s.recipe_id")
+    .leftJoin("steps_ingredients as si", "si.step_id", "s.step_id")
+    .leftJoin("ingredients as i", "i.ingredient_id", "si.step_ingredient_id")
+    .select(
+      "r.recipe_id",
+      "r.recipe_name",
+      "s.step_id",
+      "s.step_number",
+      "s.instructions",
+      "i.ingredient_id",
+      "i.ingredient_name",
+      "si.quantity"
+    )
+    .where("r.recipe_id", recipe_id);
 
   return recipeRows;
 }

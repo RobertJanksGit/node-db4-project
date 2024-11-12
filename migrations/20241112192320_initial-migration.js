616b482eff10e7fc67baf89956fcbda346ad709a
Robert Jank <robert.jank@yahoo.com>
2024-11-12 13:56:56 -0600

    wip

--- migrations/20241112192320_initial-migration.js
+++ migrations/20241112192320_initial-migration.js
@@ -13,11 +13,38 @@ exports.up = async function (knex) {
       ingredient.string("ingredient_name", 200).notNullable().unique();
       ingredient.string("ingredient_unit", 50);
     })
-    .createTable("steps", (table) => {
-      table.increments();
+    .createTable("steps", (step) => {
+      step.increments("step_id");
+      step.string("instructions", 200).notNullable();
+      step.integer("step_number").notNullable();
+      step
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("recipe_id")
+        .inTable("recipes")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
     })
-    .createTable("steps_ingredients", (table) => {
-      table.increments();
+    .createTable("steps_ingredients", (steps_ingredient) => {
+      steps_ingredient.increments("step_ingredient_id");
+      steps_ingredient.float("quantity").notNullable();
+      steps_ingredient
+        .integer("step_id")
+        .unsigned()
+        .notNullable()
+        .references("step_id")
+        .inTable("steps")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+      steps_ingredient
+        .integer("ingredient_id")
+        .unsigned()
+        .notNullable()
+        .references("ingredient_id")
+        .inTable("ingredients")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
     });
 };
 
